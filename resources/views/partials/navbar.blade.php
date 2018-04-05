e47d3f5e26cb97e4edf1f941b41d3cf07fb5e46f
Lucas Ferreira <lucasferreira@rits.com.br>
2018-04-05 14:14:03 -0300

    Adds menu toggle button

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,9 @@
 <div class="row border-bottom">
     <nav class="navbar navbar-static-top white-bg" role="navigation" style="margin-bottom: 0">
-        @yield('navbar')
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+            @yield('navbar-header')
+        </div>
         @yield('navbar-links')
     </nav>
 </div>
