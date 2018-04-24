22e35eb2a2dec3cb6291891b3f557f9f74e6c846
Lucas Ferreira <lucasferreira@rits.com.br>
2018-04-24 10:06:10 -0300

    Removes all form limitations

--- resources/views/partials/crud/edit.blade.php
+++ resources/views/partials/crud/edit.blade.php
@@ -15,26 +15,6 @@
 
 @section('content')
     {!! Form::model($instance, ['url' => $instance->route('update'), 'method' => 'put', 'data-validation' => $instance->hasRoute('validation') ? $instance->route('validation') : '']) !!}
-        <div class="row">
-            <div class="col-lg-8">
-                @component('ace::components.box')
-                    @slot('title', crudAction($type, 'edit'))
-                    @if (! isset($hideFooter) || ! $hideFooter)
-                        @slot('footer')
-                            <div class="text-right">
-                                {!! Form::submit(crudAction($type, 'save'), ['class' => 'btn btn-primary']) !!}
-                            </div>
-                        @endslot
-                    @endif
-
-                    @yield('form')
-                @endcomponent
-
-                @yield('after-form')
-            </div>
-            <div class="col-lg-4">
-                @yield('aside')
-            </div>
-        </div>
+        @yield('form')
     {!! Form::close() !!}
 @stop
--- resources/views/partials/crud/new.blade.php
+++ resources/views/partials/crud/new.blade.php
@@ -10,26 +10,6 @@
 
 @section('content')
     {!! Form::model($instance, ['url' => $instance->route('create'), 'method' => 'post', 'data-validation' => $instance->hasRoute('validation') ? $instance->route('validation') : '']) !!}
-        <div class="row">
-            <div class="col-lg-8">
-                @component('ace::components.box')
-                    @slot('title', crudAction($type, 'new'))
-                    @if (! isset($hideFooter) || ! $hideFooter)
-                        @slot('footer')
-                            <div class="text-right">
-                                {!! Form::submit(crudAction($type, 'save'), ['class' => 'btn btn-primary']) !!}
-                            </div>
-                        @endslot
-                    @endif
-
-                    @yield('form')
-                @endcomponent
-
-                @yield('after-form')
-            </div>
-            <div class="col-lg-4">
-                @yield('aside')
-            </div>
-        </div>
+        {!! Form::submit(crudAction($type, 'save'), ['class' => 'btn btn-primary']) !!}
     {!! Form::close() !!}
 @stop
